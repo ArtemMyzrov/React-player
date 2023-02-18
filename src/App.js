f62d65e5777bb22e03584788d3814a1d7ba8f82e
artemmyzrov <a.myzrov@yandex.ru>
2023-02-18 12:31:43 +0400

    init: start project

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css'
+import TodoForm from './components/Todos/TodoForm'
+
+function App() {
+  return (
+    <>
+      <h1 style={backgroundColor:red}>Todo App</h1>
+      <div className="App">
+        <TodoForm />
+      </div>
+    </>
+  )
+}
+
+export default App
